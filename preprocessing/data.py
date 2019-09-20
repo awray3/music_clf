4cb0dc8b72113b86ad039b677e54c2214d54a447
awray-ubuntu <andrewwray2718@gmail.com>
2019-09-19 18:40:53 -0700

    Progress on dataset class

--- preprocessing/data.py
+++ preprocessing/data.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
+from torch.utils.data import Dataset
+
+class Mp3Dataset(Dataset):
+
+    def __init__(self, csv_path, file_path)
+
+        csvData = pd.read_csv(csv_path)
+        self.file_names = []
+        self.labels = []
+
+        # go through the dataframe and import the filenames and genre labels
+        for i in range(0, len(csvData)):
+            self.
+    
 
 
 
