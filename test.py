c737b66287ad63356d52f4e6feaa64de6a423091
awray-ubuntu <andrewwray2718@gmail.com>
2019-09-19 18:06:44 -0700

    Clean for later commits

--- test.py
+++ test.py
@@ -1,34 +1,10 @@
 """
-Main script for testing preprocessing module.
+Tutorial for pytorch and torchaudio
 """
-import os
-from time import time
 import pandas as pd
+import numpy as np
+import torch
+import torchaudio
 
-from preprocessing.tracks.paths import create_mp3_objects
-from preprocessing.convert import prepare_mp3s_and_labels
+audio_dir = os.path.join('data','fma_small','002')
 
-print("Loading metadata.")
-meta_fp = os.path.join('data', 'fma_metadata', 'medium_track_info.csv')
-
-# subdir = str(input('Enter 3-digit subdir: '))
-# subdir = '133'
-# subdir = '054'
-# audio_dir = os.path.join('data', 'fma_small', subdir)
-audio_dir = os.path.join('data', 'fma_medium')
-
-df = pd.read_csv(meta_fp, index_col=0)
-
-# 080391
-# df.drop(df.loc[df['track_id'] == 80391, :].index, axis=0, inplace=True)
-#hopefully the issue won't happen until the "mass issue"
-# print(df.head())
-
-mp3_list = create_mp3_objects(audio_dir, df)
-print("Finished creating the mp3 objects.", "Beginning Librosa loading.")
-t1 = time()
-X, y, split_labels = prepare_mp3s_and_labels(mp3_list, duration=1.0)
-
-t2 = time()
-
-print(f"Completed preprocessing in {t2-t1}")
