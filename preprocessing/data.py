9dd09588231a9e7d51d457d3e765a2d24c4cf3d3
awray-ubuntu <andrewwray2718@gmail.com>
2019-09-24 22:23:10 -0700

    Fix dataset class

--- preprocessing/data.py
+++ preprocessing/data.py
@@ -1,20 +1,35 @@
+import os
 import pandas as pd
-import numpy as np
+
 from torch.utils.data import Dataset
+import torchaudio
+import torch
 
 class Mp3Dataset(Dataset):
 
-    def __init__(self, csv_path, file_path)
+    def __init__(self, csv_path, audio_path):
 
-        csvData = pd.read_csv(csv_path)
-        self.file_names = []
-        self.labels = []
+        metadata = pd.read_csv(csv_path)
+        self.audio_path = audio_path
+        self.IDs = metadata.track_id.astype(str).to_list()
+        self.genre_list = metadata.genre.to_list()
 
-        # go through the dataframe and import the filenames and genre labels
-        for i in range(0, len(csvData)):
-            self.
-    
-
+    def __len__(self):
+        return len(self.IDs)
 
 
+    def __getitem__(self, index):
+        ID = self.IDs[index]
+        genre = self.genre_list[index]
 
+        waveform, sr = torchaudio.load(self.get_path_from_ID(ID))
+
+        return waveform, sr, genre
+
+    def get_path_from_ID(self, ID):
+        """
+        Gets
+        """
+        track_id = ID.zfill(6)
+
+        return os.path.join(self.audio_path, track_id[:3], track_id + '.mp3')
