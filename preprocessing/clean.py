8052e94656b19733ef1379fea038a0d4e2ddce99
awray_mac <andrewwray2718@gmail.com>
2019-09-28 10:02:53 -0700

    Complete cleaning script
    The cleaning script is now complete. It scans the directory and attempts
    to load info on each song. The process is now very fast due to using
    the torchaudio.info() rather than torchaudio.load() to check for
    corruption.
    
    Corrupted files are now listed. The user will be asked to confirm
    deleting the selected files.
    
    Lastly, some of the files seem to show a strange warning:
    MAD lost sync
    These are not throwing exceptions, so for now I will assume that
    there is no trouble with these files.

--- preprocessing/clean.py
+++ preprocessing/clean.py
@@ -3,26 +3,46 @@ The purpose of this script is to clean the mp3 directory of
 damaged mp3 files that don't load.
 """
 import os
-
 import torchaudio
 
 def clean_mp3_directory(audio_dir):
+    """
+    module for checking corruptness of mp3s. Loops over the given 
+    directory attempting to load info on the tracks. If loading fails,
+    the user will be asked if they want to remove the corrupt files 
+    from their system.
+    """
+    corrupted_list = []
     for root, dirs, files in os.walk(audio_dir):
         if dirs == []:
             for f in files:
                 try:
                     print('Loading ' + f, end='\r')
-                    torchaudio.load(os.path.join(root, f))
+                    # torchaudio.load(os.path.join(root, f))
+                    torchaudio.info(os.path.join(root, f))
                 except RuntimeError as e:
                     print("Unable to load file" + f + ", " + repr(e))
-                    delete_option = input('Delete this file?')
-                    if delete_option == 'yes':
-                        os.remove(os.path.join(root,f))
+                    corrupted_list.append(os.path.join(root, f))
 
 
+    if len(corrupted_list) == 0:
+        print('No mp3s were found to be corrupted. Exiting cleaning.')
 
+    else:
+        print('The following files were found to be corrupted:')
+        for fpath in corrupted_list:
+            print(fpath)
+        delete_option = input('Would you like to delete these files? (yes/no) ')
+        if delete_option == 'yes':
+            for fpath in corrupted_list:
+                os.remove(fpath)
+                print('Removed ' + fpath)
 
+        print('Exiting cleaning.')
 
 if __name__ == '__main__':
     audio_dir = 'data/fma_small'
+    clean_mp3_directory(audio_dir)
+
+    audio_dir = 'data/fma_medium'
     clean_mp3_directory(audio_dir)
