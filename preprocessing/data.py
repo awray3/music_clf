641344153b96fd27bd5fe464b2220a94cfad7972
awray_mac <andrewwray2718@gmail.com>
2019-09-28 10:34:33 -0700

    Add cleaning module to Mp3dataset class

--- preprocessing/data.py
+++ preprocessing/data.py
@@ -7,13 +7,12 @@ from torch.utils.data import Dataset
 import torch
 import torchaudio
 
-
 sr = 22050
 
 n_mels = 64
 fft_window_pts = 512
-fft_window_dur = fft_window_pts * 1.0 / sr # 23 ms window length
-hop_size = fft_window_pts // 2 # 50% overlap between consecutive frames
+fft_window_dur = fft_window_pts * 1.0 / sr  # 23 ms window length
+hop_size = fft_window_pts // 2  # 50% overlap between consecutive frames
 
 
 class Mp3Dataset(Dataset):
@@ -28,8 +27,8 @@ class Mp3Dataset(Dataset):
         self.audio_path = audio_path
         self.IDs = df.track_id.astype(str).to_list()
         self.genre_list = df.genre.to_list()
-        self.genre_dict = dict(zip(set(self.genre_list),
-                                   range(len(set(self.genre_list)))))
+        self.genre_dict = dict(
+            zip(set(self.genre_list), range(len(set(self.genre_list)))))
         self.duration = duration
 
         # create the chain of preprocessing
@@ -38,9 +37,10 @@ class Mp3Dataset(Dataset):
         self.E.append_effect_to_chain("rate", [sr])
         self.E.append_effect_to_chain("channels", ["1"])
 
+        self.clean_IDs()
+
     def __len__(self):
         return len(self.IDs)
-
 
     def __getitem__(self, index):
         ID = self.IDs[index]
@@ -49,7 +49,7 @@ class Mp3Dataset(Dataset):
 
         self.E.set_input_file(self.get_path_from_ID(ID))
 
-        waveform, _ = self.E.sox_build_flow_effects() # size: [1, len * sr]
+        waveform, _ = self.E.sox_build_flow_effects()  # size: [1, len * sr]
 
         # padding in case the waveform is too short
         if waveform.size()[1] < self.duration * sr:
@@ -58,11 +58,10 @@ class Mp3Dataset(Dataset):
             new_waveform[:, :waveform.size()[1]] = waveform
             waveform = new_waveform
         # convert to melspec
-        melspec = torchaudio.transforms.MelSpectrogram(
-            sample_rate=sr,
-            n_fft=fft_window_pts,
-            hop_length=hop_size,
-            n_mels=n_mels)(waveform)
+        melspec = torchaudio.transforms.MelSpectrogram(sample_rate=sr,
+                                                       n_fft=fft_window_pts,
+                                                       hop_length=hop_size,
+                                                       n_mels=n_mels)(waveform)
 
         # transpose the last two coordinates so that time is interpreted
         # as channels
@@ -78,3 +77,22 @@ class Mp3Dataset(Dataset):
 
         return os.path.join(self.audio_path, track_id[:3], track_id + '.mp3')
 
+    def clean_IDs(self):
+        """
+        Checks if each of the files in the ID list exists or not.
+        Removes these elements from the index if not found.
+        """
+
+        files_not_found = []
+        for ID in self.IDs:
+            if os.path.exists(self.get_path_from_ID(ID)):
+                continue
+            else:
+                files_not_found.append(ID)
+
+        if files_not_found != []:
+            print('The following IDs will be removed from the ID list.')
+            for ID in files_not_found:
+                print(ID)
+
+            self.IDs = [ID for ID in self.IDs if ID not in files_not_found]
