97135e6ae2cc3f108bf48cebef8fd53a737060b1
awray-ubuntu <andrewwray2718@gmail.com>
2019-09-29 12:25:14 -0700

    Module-ize the script

--- create_fma_df.py
+++ create_fma_df.py
@@ -2,14 +2,19 @@ import os
 
 from preprocessing.fma_metadata import get_fma_csv
 
-meta_dir = os.path.join('data', 'fma_metadata')
-meta_fp = os.path.join(meta_dir, 'tracks.csv')
+def create_fma_df(meta_dir, meta_fp, fma_option)
 
-fma_size = input('Select fma size (small or medium): ')
-
-if fma_size != 'small' and fma_size != 'medium':
+if fma_option not in ['small', 'medium']:
 	raise ValueError('Need to select small or medium.')
 
 df = get_fma_csv(meta_fp, fma_size)
 
 df.to_csv(os.path.join(meta_dir, fma_size + '_track_info.csv'))
+
+if __name__ == '__main__':
+
+    meta_dir = os.path.join('data', 'fma_metadata')
+    meta_fp = os.path.join(meta_dir, 'tracks.csv')
+
+    fma_option = input('Select fma size (small or medium): ')
+    create_fma_df(meta_dir, meta_fp, fma_option)
