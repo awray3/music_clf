1d22976d0e23441880d1ceadb1aaecaf106996de
awray-ubuntu <andrewwray2718@gmail.com>
2019-09-29 10:17:38 -0700

    Reformat code

--- preprocessing/clean.py
+++ preprocessing/clean.py
@@ -1,15 +1,16 @@
 """
-The purpose of this script is to clean the mp3 directory of 
+The purpose of this script is to clean the mp3 directory of
 damaged mp3 files that don't load.
 """
 import os
 import torchaudio
 
+
 def clean_mp3_directory(audio_dir):
     """
-    module for checking corruptness of mp3s. Loops over the given 
+    module for checking corruptness of mp3s. Loops over the given
     directory attempting to load info on the tracks. If loading fails,
-    the user will be asked if they want to remove the corrupt files 
+    the user will be asked if they want to remove the corrupt files
     from their system.
     """
     corrupted_list = []
@@ -24,7 +25,6 @@ def clean_mp3_directory(audio_dir):
                     print("Unable to load file" + f + ", " + repr(e))
                     corrupted_list.append(os.path.join(root, f))
 
-
     if len(corrupted_list) == 0:
         print('No mp3s were found to be corrupted. Exiting cleaning.')
 
@@ -32,13 +32,15 @@ def clean_mp3_directory(audio_dir):
         print('The following files were found to be corrupted:')
         for fpath in corrupted_list:
             print(fpath)
-        delete_option = input('Would you like to delete these files? (yes/no) ')
+        delete_option = input(
+            'Would you like to delete these files? (yes/no) ')
         if delete_option == 'yes':
             for fpath in corrupted_list:
                 os.remove(fpath)
                 print('Removed ' + fpath)
 
         print('Exiting cleaning.')
+
 
 if __name__ == '__main__':
     audio_dir = 'data/fma_small'
