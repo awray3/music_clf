9fbc227201f142c0f60ce74dc16c6d6f9c611667
awray-ubuntu <andrewwray2718@gmail.com>
2019-11-02 20:24:00 -0700

    Finish preprocessing script

--- preprocess.py
+++ preprocess.py
@@ -1,48 +1,81 @@
 """
 Preprocessing an mp3 dataset.
 
-Inputs: 
+Inputs:
     * meta_dir: directory where the FMA metadata is contained.
     * meta_path - file path to the .csv file containing info about
     track id, genre, and optionally the split label.
     * audio_dir - folder containing all mp3 files to be loaded.
-    * fma_size (default: None) - string for the size of FMA dataset 
+    * fma_size (default: None) - string for the size of FMA dataset
     to process. If left to None, no csv is created.
 
 
 Note: if using the FMA dataset, each song only lasts up to 30 seconds.
-
-This script performs the following options
 """
 import os
 import pandas as pd
 
 from preprocessing.clean import clean_mp3_directory
-from preprocessing.fma import create_csv
+from preprocessing.fma import create_df
 
 
 def main(meta_dir: str,
          meta_path: str,
          audio_dir: str,
-         fma_size: str=None) -> None:
+         fma_size: str = None) -> None:
+    """
+    Main function for the preprocessing script. This is a
+    wrapper around create_df and clean_mp3_directory that manages
+    the rows in the resulting dataframe.
+
+    This script, when the fma_size is specified, will
+        - create a metadata dataframe appropriate for loading data from the
+            metadata csv
+        - clean the mp3 directory, asking for permission to delete
+        the files that were found to be corrupted
+        - remove rows from the metadata dataframe
+        - save the csv to the metadata directory.
+
+    When the fma_size is not specified this will delete the corrupted files
+    (again with permission) and just print out a list of files.
+    """
 
     print('Beginning preprocessing.')
 
     # if using FMA dataset, create metadata csv if it doesn't already exist.
     if fma_size:
         print('You have selected to use one of the FMA datasets.')
+
+        # check that the tracks.csv file exists
         if os.path.isfile(meta_path):
             if fma_size not in ['small', 'medium']:
                 raise NotImplementedError('Please choose small or medium.')
-            create_csv(fma_size, meta_path=meta_path, output_dir=meta_dir)
+            meta_df = create_df(fma_size, meta_path=meta_path)
+            print(f"number rows: {len(meta_df)}")
         else:
             raise ValueError('Track csv path not found.')
 
     # run cleaning script on the audio directory.
-    meta_df = pd.read_csv(meta_path)
+
     if os.path.isdir(audio_dir):
-        meta_df = clean_mp3_directory(audio_dir, meta_df)
-        meta_df.to_csv(meta_path)
+        corrupted_mp3_labels = clean_mp3_directory(audio_dir)
+
+        print(corrupted_mp3_labels)
+        if fma_size:
+
+            meta_df = meta_df.loc[
+                ~meta_df['track_id'].isin(corrupted_mp3_labels), :]
+
+            print(f"number rows: {len(meta_df)}")
+
+            # output meta_df to csv
+            meta_df.to_csv(os.path.join(meta_dir, fma_size +
+                                        '_track_info.csv'), index=False)
+
+        else:
+            print("Names of deleted files: ")
+            print(corrupted_mp3_labels)
+
     else:
         raise ValueError('Audio directory not found.')
 
@@ -55,5 +88,5 @@ def main(meta_dir: str,
 if __name__ == '__main__':
     TRACKS_PATH = os.path.join('data', 'fma_metadata', 'tracks.csv')
     META_DIR = os.path.join('data', 'fma_metadata')
-    AUDIO_DIR = os.path.join('data', 'fma_small', '000')
+    AUDIO_DIR = os.path.join('data', 'fma_small')
     main(META_DIR, TRACKS_PATH, AUDIO_DIR, fma_size='small')
--- train.py
+++ train.py
@@ -1,3 +1,77 @@
 """
 Main script for training and analyzing models.
 """
+# import os
+
+import numpy as np
+import pandas as pd
+
+import torch
+import torchaudio
+from models import Baseline_cnn
+from preprocessing.data import Mp3Dataset
+from torch.utils.data import DataLoader
+
+# audio path
+
+# small fma dataset
+meta_path = "./data/fma_metadata/small_track_info.csv"
+audio_path = "./data/fma_small"
+
+# medium fma dataset
+# meta_path = "./data/fma_metadata/medium_track_info.csv"
+# audio_path = "./data/fma_medium"
+
+# load the metadata csv and shuffle it.
+meta_df = pd.read_csv(meta_path).sample(frac=1, random_state=1)
+
+# shuffle into 60/20/20 train valid test split
+train_df, valid_df, test_df = np.split(
+        meta_df.sample(frac=1, random_state=1),
+        [int(.6 * len(meta_df)), int(.8 * len(meta_df))]
+        )
+
+# parameters
+params = {'batch_size': 16, 'shuffle': True, 'num_workers': 0}
+learning_rate = 1e-2
+num_epochs = 2
+
+# turn on sox
+torchaudio.initialize_sox()
+
+# Data generators
+training_set = Mp3Dataset(train_df, audio_path, 1.0)
+training_generator = DataLoader(training_set, **params)
+
+# validation_set = Mp3Dataset(valid_df, audio_path, 1.0)
+# validation_generator = DataLoader(validation_set, **params)
+
+# test_set = Mp3Dataset(test_df, audio_path, 1.0)
+# test_generator = DataLoader(test_set, **params)
+
+# Model stuff
+model = Baseline_cnn()
+
+# create model instance
+
+loss_fn = torch.nn.CrossEntropyLoss()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+# two epochs for now
+for t in range(num_epochs):
+    for idx, (batch_mel, batch_genre) in enumerate(training_generator):
+        # forward pass:
+        pred_genre = model(batch_mel)
+
+        # calculate loss
+        loss = loss_fn(pred_genre, batch_genre)
+        optimizer.zero_grad()
+
+        loss.backward()
+        optimizer.step()
+        print(f'Finished step {idx} of {6400//16}.', end='\r')
+    print(loss)
+
+# close sox
+torchaudio.shutdown_sox()
