74beb01dd3f63c8416c523a8551344624f12626c
awray_mac <andrewwray2718@gmail.com>
2019-11-26 15:36:36 -0800

    Add one-hot encoding to meta_df
    Added one-hot encoding and renamed `reduced_meta` to `meta_df`.
    Also added a quick logistic regression to the list of models
    to get a keras model working. It works, but preprocessing each
    mp3 file takes too long. Setting out now to run the preprocessing into
    images separately.

--- utilities.py
+++ utilities.py
@@ -1,11 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
 from librosa.feature import melspectrogram
 from librosa import power_to_db, load
 from librosa.display import specshow
-from tensorflow.keras.utils import Sequence
+from tensorflow.keras.utils import Sequence 
 
 def view_melspec(source, sr):
     plt.figure(figsize=(10, 4))
@@ -35,22 +34,20 @@ def read_metadata_file(path, all_filepaths, bad_filepaths):
     
     df.columns = ['track_id', 'genre']
     
-    # encode the columns
-    enc = LabelEncoder()
-    
-    df['genre_enc'] = enc.fit_transform(df['genre'].to_numpy().reshape(-1,1))
-    
     # add filepaths to the dataframe
-    df['path'] = all_filepaths
+    df['raw_path'] = all_filepaths
     
     # Remove bad mp3s from the dataframe so that we skip them.
-    if df.path.isin(bad_filepaths).sum():
+    if df.raw_path.isin(bad_filepaths).sum():
         df.drop(
-            df.loc[df.path.isin(bad_filepaths), :].index,
+            df.loc[df.raw_path.isin(bad_filepaths), :].index,
             inplace=True
         )
         print(f"Dropped {len(bad_filepaths)} rows from the dataframe.")
         
+    # one-hot encoding of columns
+    df = pd.concat([df, pd.get_dummies(df.genre)], axis=1)
+
     return df
 
 
@@ -61,8 +58,8 @@ class Batch_generator(Sequence) :
     """
   
     def __init__(self, meta_df, batch_size, sr, duration):
-        self.music_filepaths = meta_df['path'].to_list()
-        self.labels = meta_df['genre_enc'].to_numpy()
+        self.mp3_paths = meta_df['raw_path'].to_list()
+        self.labels = meta_df.loc[:, meta_df.genre.unique()].to_numpy()
         self.batch_size = batch_size
         self.sr = sr
         self.duration = duration
@@ -71,12 +68,12 @@ class Batch_generator(Sequence) :
         """
         Return number of batches.
         """
-        return (np.ceil(len(self.music_filepaths) / float(self.batch_size))).astype(np.int)
+        return (np.ceil(len(self.mp3_paths) / float(self.batch_size))).astype(np.int)
   
     def __getitem__(self, idx):
         
-        batch_x = self.music_filepaths[idx * self.batch_size : (idx + 1) * self.batch_size]
-        batch_y = self.labels[idx * self.batch_size : (idx + 1) * self.batch_size]
+        batch_x = self.mp3_paths[idx * self.batch_size : (idx + 1) * self.batch_size]
+        batch_y = self.labels[idx * self.batch_size : (idx + 1) * self.batch_size, :]
         
         return self._stack_melspecs(batch_x), batch_y
     
