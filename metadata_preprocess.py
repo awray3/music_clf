9c8e09d67452e08abf9a1c5b85504ca201d772c9
Andrew Wray <andrewwray2718@gmail.com>
2020-07-19 14:33:14 -0700

    Refactor notebook

--- metadata_preprocess.py
+++ metadata_preprocess.py
@@ -1,33 +1,104 @@
+""" processes metadata into a simpler metdata file."""
+
 import os
 
-from librosa.util.files import find_files
+import pandas as pd
+import librosa
 from torchaudio import info
 
-from utilities import read_metadata_file
+from utilities import id_from_path
 
-# setup audio directories
-audio_dir = os.path.join(".", "data", "fma_small/")
-raw_metadata_path = os.path.join(".","data", "fma_metadata", "tracks.csv")
-target_metadata_path = os.path.join(".","data", "fma_metadata", "meta_df.csv")
 
-# check if the path exists and exit if it doesn't.
-if not os.path.exists(raw_metadata_path):
-    raise FileNotFoundError("Data directory or metadata file not found; check it is in the correct place.")
+def read_metadata_file(path, all_filepaths, bad_filepaths):
+    """
+    read in the raw metadata file and trim it.
+    """
+    all_metadata = pd.read_csv(path, header=[0, 1], index_col=0)
 
-# check if the metadata file has already been created
-if os.path.exists(target_metadata_path):
-    raise FileExistsError("Metadata file exists. Double check that the preprocessing hasn't already been executed.")
+    cols_to_keep = [('track', 'genre_top')]
 
-filepaths = find_files(audio_dir)
+    # This will be the main dataframe for here on out:
+    df = all_metadata.loc[
+        all_metadata[('set', 'subset')] == 'small',
+        cols_to_keep
+    ]
 
-# Extract bad files using torchaudio
-bad_files = []
-for file in filepaths:
-    try:
-        info_obj = info(file)[0]
-    except RuntimeError:
-        bad_files.append(file)
+    df.reset_index(inplace=True)
+    df.columns = ['track_id', 'genre']
 
-# runs the preprocessing of the data file and conversion to melspecs
-meta_df = read_metadata_file(raw_metadata_path, filepaths, bad_files)
-meta_df.to_csv(target_metadata_path)
+    # add filepaths to the dataframe
+    df['mp3_path'] = all_filepaths
+
+    # Remove bad mp3s from the dataframe so that we skip them.
+    if df.mp3_path.isin(bad_filepaths).sum():
+        df.drop(
+            df.loc[df.mp3_path.isin(bad_filepaths), :].index,
+            inplace=True
+        )
+        print(f"Dropped {len(bad_filepaths)} rows from the dataframe.")
+
+    df['duration'] = df['mp3_path'].apply(lambda x:
+                                          librosa.get_duration(filename=x))
+
+    return df
+
+
+def attach_onehot_encoding(df):
+    """
+    Append the onehot representation of `column` onto the right end
+    of the array df.
+    """
+
+    df = pd.concat([df, pd.get_dummies(df.genre)], axis=1)
+
+    return df
+
+
+if __name__ == "__main__":
+    # setup audio directories
+    audio_dir = os.path.join(".", "data", "fma_small/")
+    raw_metadata_path = os.path.join(".", "data", "fma_metadata", "tracks.csv")
+    target_metadata_path = os.path.join(
+        ".", "data", "fma_metadata", "meta_df.csv")
+
+    melspec_dir = os.path.join(".", "data", "fma_small_melspecs")
+
+    #  Make the folders for the given models:
+    if not os.path.exists("./models/"):
+        os.mkdir("./models/")
+
+
+    # check if the path exists and exit if it doesn't.
+    if not os.path.exists(raw_metadata_path):
+        raise FileNotFoundError(
+            "Data directory or metadata file not found; check it is in the correct place.")
+
+    # check if the metadata file has already been created
+    if os.path.exists(target_metadata_path):
+        raise FileExistsError(
+            "Metadata file exists. Double check that the \
+            preprocessing hasn't already been executed.")
+
+    filepaths = librosa.util.find_files(audio_dir)
+
+    # Extract bad files using torchaudio
+    bad_files = []
+    for file in filepaths:
+        try:
+            info_obj = info(file)[0]
+        except RuntimeError:
+            bad_files.append(file)
+
+    # runs the preprocessing of the data file and conversion to melspecs
+    meta_df = read_metadata_file(raw_metadata_path, filepaths, bad_files)
+
+    # delete samples less than 30 seconds
+    meta_df.drop(meta_df.loc[(meta_df.duration < 28),
+                             :].index, axis=0, inplace=True)
+
+    meta_df["mel_path"] = meta_df["mp3_path"].apply(
+        lambda path: os.path.join(melspec_dir, id_from_path(path) + ".npz"))
+
+    meta_df = attach_onehot_encoding(meta_df)
+
+    meta_df.to_csv(target_metadata_path)
--- utilities.py
+++ utilities.py
@@ -1,33 +1,33 @@
+"""
+Collection of helpful utilities for visualization and processing data
+"""
+
 import os
-import glob
 
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
 
 from librosa.feature import melspectrogram
-from librosa import power_to_db, load, get_duration
+from librosa import power_to_db
 from librosa.display import specshow
 
-from tensorflow.keras.models import load_model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
-from tensorflow.keras.utils import Sequence 
-from sklearn.metrics import confusion_matrix, classification_report
 
 def view_melspec(source, sr):
+    """quickly plot a melspectrogram."""
+
     plt.figure(figsize=(10, 4))
     S = melspectrogram(source, sr=sr, n_mels=200)
     S_dB = power_to_db(S, ref=np.max)
     specshow(S_dB, x_axis='time',
-                             y_axis='mel', sr=sr,
-                             fmax=4092)
+             y_axis='mel', sr=sr,
+             fmax=4092)
     plt.colorbar(format='%+2.0f dB')
     plt.title('Melspectrogram')
     plt.tight_layout()
     plt.show()
 
-def plot_sample(genre, meta_df, nrow=3, waveform=False, **kwargs):
+
+def plot_sample(genre, meta_df, nrow=3, **kwargs):
     """
     Plots 2*nrow randomly chosen spectrograms
     (or waveforms if set to True) from the given genre.
@@ -42,20 +42,14 @@ def plot_sample(genre, meta_df, nrow=3, waveform=False, **kwargs):
     for i in range(2 * nrow):
         plt.subplot(nrow, 2, i+1)
 
-
-        if waveform:
-            wave = librosa.load(samples["mp3_path"].iloc[i], **kwargs)
-            plt.xlabel("Sample Position")
-            plt.ylabel("Amplitude")
-        else:
-            #Load the melspectrogram
-            S = np.load(samples["mel_path"].iloc[i])['arr_0']
-            #convert from power to db 
-            S_dB = power_to_db(S, ref=np.max)
-            specshow(S_dB, x_axis="time", y_axis="mel", fmax=4092,
-                    **kwargs)
-            plt.colorbar(format="%+2.0f dB")
-            plt.title("Track id " + str(samples["track_id"].iloc[i]))
+        # Load the melspectrogram
+        S = np.load(samples["mel_path"].iloc[i])['arr_0']
+        # convert from power to db
+        S_dB = power_to_db(S, ref=np.max)
+        specshow(S_dB, x_axis="time", y_axis="mel", fmax=4092,
+                 **kwargs)
+        plt.colorbar(format="%+2.0f dB")
+        plt.title("Track id " + str(samples["track_id"].iloc[i]))
     plt.show()
 
 
@@ -65,15 +59,6 @@ def id_from_path(mp3_path):
     """
     return os.path.split(mp3_path)[1][:-4]
 
-def attach_onehot_encoding(df, column_name):
-    """
-    Append the onehot representation of `column` onto the right end
-    of the array df.
-    """
-
-    df = pd.concat([df, pd.get_dummies(df.genre)], axis=1)
-
-    return df
 
 def mp3_to_mel_path(mp3_path, melspec_dir):
     """
@@ -90,209 +75,43 @@ def mp3_to_mel_path(mp3_path, melspec_dir):
             f"Did not find a melspectrogram with id {id_from_path(mp3_path)}"
         )
 
-def read_metadata_file(path, all_filepaths, bad_filepaths):
-    all_metadata = pd.read_csv(path, header=[0,1], index_col=0)
-    
-    cols_to_keep = [('track', 'genre_top')]
-
-    # This will be the main dataframe for here on out:
-    df = all_metadata.loc[
-                all_metadata[('set', 'subset')] == 'small',
-                cols_to_keep
-    ]
-
-    df.reset_index(inplace=True)
-    
-    df.columns = ['track_id', 'genre']
-    
-    # add filepaths to the dataframe
-    df['mp3_path'] = all_filepaths
-    
-    # Remove bad mp3s from the dataframe so that we skip them.
-    if df.mp3_path.isin(bad_filepaths).sum():
-        df.drop(
-            df.loc[df.mp3_path.isin(bad_filepaths), :].index,
-            inplace=True
-        )
-        print(f"Dropped {len(bad_filepaths)} rows from the dataframe.")
-
-    df['duration'] = df['mp3_path'].apply(lambda x:
-            get_duration(filename=x))
-
-    return df
 
 def standardize_2d_array(X):
     """ This is a min-max scaler for a 2D numpy array. """
     return (X-X.min())/(X.max()-X.min())
 
-def subset_data(meta_df, genre_list, n_samples):
+
+def subset_data(meta_df, genre_list, n_samples=None):
     """
     Loads melspecs from meta_df from the given genres and normalizes them.
     Only returns a random subset of size n_samples.
     """
-    
-    meta_sub_df = meta_df.loc[meta_df["genre"].isin(genre_list), :].sample(n_samples)
-    
-    all_melspecs = []  
+
+    if n_samples:
+        meta_sub_df = meta_df.loc[meta_df["genre"].isin(genre_list),
+                                  :].sample(n=n_samples)
+    else:
+        meta_sub_df = meta_df.loc[meta_df["genre"].isin(genre_list),
+                                  :].sample(frac=1)
+
+    all_melspecs = []
     for path in meta_sub_df.mel_path.values:
         if not np.any(np.isnan(
-                            loaded_array := standardize_2d_array(np.load(path)["arr_0"])
-                            )
-                        ):
+                loaded_array := standardize_2d_array(np.load(path)["arr_0"])
+        )):
             all_melspecs.append(loaded_array)
-        else: 
-            print(f"track with id {id_from_path(path)} was non-standardizable. Dropping from dataset.")
+        else:
+            print(
+                f"track with id {id_from_path(path)} was non-standardizable. \
+                Dropping from dataset.")
             meta_sub_df.drop(
-                    meta_sub_df.loc[meta_sub_df.mel_path==path,:].index,
-                    inplace=True
-                    )
+                meta_sub_df.loc[meta_sub_df.mel_path == path, :].index,
+                inplace=True
+            )
 
-
-        
-    X = np.stack(all_melspecs) 
+    X = np.stack(all_melspecs)
     y = meta_sub_df[genre_list].to_numpy()
 
     assert X.shape[0] == len(y)
-    
+
     return X, y
-
-
-class MyModel:
-    """
-    Wrapper class that manages training, loading, hyperparameter tuning,
-    and evaluation of a model.
-    """
-
-    def __init__(self, batch_size, model_dir, genre_labels, X_train, y_train, X_test, y_test):
-        self.batch_size = batch_size
-        self.model_dir = model_dir
-        self.genre_labels = genre_labels
-        self.X_train = X_train
-        self.y_train = y_train
-        self.X_test = X_test
-        self.y_test = y_test
-
-
-        if not os.path.exists(self.model_dir):
-            os.mkdir(self.model_dir)
-
-        self.model_path = glob.glob(os.path.join(model_dir, "*.h5"))
-        ### Currently this will fail if there isn't already a file there.
-
-        if self.model_path:
-            self.model_path = self.model_path[0]
-        else:
-            self.model_path = os.path.join(self.model_dir, "model.h5")
-
-        self.input_shape = (1,) + self.X_train.shape[1:]
-
-    def attach_model(self, model):
-        """ Attach a Keras model. """
-        self.model = model
-        self._compile()
-
-    def load_model(self):
-        """
-        Loads the model from the model directory. Alternative to passing 
-        a new model.
-        """
-        if os.path.exists(self.model_path):
-            self.model = load_model(self.model_path)
-        else:
-            raise ValueError("File Not Found. Please train the model.")
-
-    def fit(self, num_epochs, verbose=1):
-        checkpoint_callback = ModelCheckpoint(
-            self.model_path,
-            monitor="val_accuracy",
-            verbose=verbose,
-            save_best_only=True,
-            mode="max",
-        )
-        reducelr_callback = ReduceLROnPlateau(
-            monitor="val_accuracy", factor=0.5, patience=5, min_delta=0.01,
-            verbose=verbose
-        )
-        early_stop = EarlyStopping(
-            monitor="val_accuracy", patience=15, verbose=verbose, restore_best_weights=True
-        )
-
-        callbacks_list = [checkpoint_callback, reducelr_callback, early_stop]
-
-        self.history = self.model.fit(
-            x=self.X_train,
-            y=self.y_train,
-            epochs=num_epochs,
-            validation_split=0.1,
-            verbose=verbose,
-            callbacks=callbacks_list,
-        )
-
-    def summary(self):
-        return self.model.summary()
-
-    def _compile(self, loss="categorical_crossentropy"):
-        self.model.compile(
-            optimizer=Adam(lr=0.001),
-            loss=loss,
-            metrics=["accuracy"],
-        )
-
-    def evaluate(self, verbose=0, all=False):
-
-        print("Training set")
-        self.model.evaluate(
-                x=self.X_train,
-                y=self.y_train,
-                verbose=verbose)
-        print("Testing set")
-        _, accuracy = self.model.evaluate(
-                x=self.X_test,
-                y=self.y_test,
-                verbose=verbose)
-
-        return accuracy
-
-    def plot_history(self):
-        plt.plot(self.history.history["accuracy"])
-        plt.plot(self.history.history["val_accuracy"])
-        plt.title("Model accuracy")
-        plt.ylabel("Accuracy")
-        plt.xlabel("Epoch")
-        plt.legend(["Train", "Valid"], loc="upper left")
-        plt.show()
-
-        plt.savefig(os.path.join(self.model_dir, "last_training_acc.png"))
-
-        # Plot training & validation loss values
-        plt.plot(self.history.history["loss"])
-        plt.plot(self.history.history["val_loss"])
-        plt.title("Model loss")
-        plt.ylabel("Loss")
-        plt.xlabel("Epoch")
-        plt.legend(["Train", "Valid"], loc="upper left")
-        plt.show()
-        plt.savefig(os.path.join(self.model_dir, "last_training_loss.png"))
-
-    def confusion_matrix(self, print_labels=True):
-
-        y_pred = np.argmax(self.model.predict(self.X_test), axis=1)
-        classes = np.argmax(self.y_test, axis=1)
-
-
-        print(confusion_matrix(classes, y_pred))
-
-    def classification_report(self):
-
-        y_pred = np.argmax(self.model.predict(self.X_test), axis=1)
-        classes = np.argmax(self.y_test, axis=1)
-
-        print(
-            classification_report(
-                classes, y_pred,
-                target_names=self.genre_labels
-            )
-        )
-
-
-
