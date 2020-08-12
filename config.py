1943d56a7e1319fbbbfb42343539eccb6a6e6922
Andrew Wray <andrewwray2718@gmail.com>
2020-08-12 11:15:11 -0700

    Make number of genres a global variable

--- config.py
+++ config.py
@@ -13,6 +13,8 @@ DATA_OPTION = "mfcc"
 # processed data location
 JSON_PATH = os.path.join(DATA_DIR, DATA_OPTION + ".json")
 
+# dataset properties
+NUM_GENRES = 10
 
 # waveform properties
 SAMPLE_RATE = 22050
