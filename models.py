c486720bce403867cfa9d6f63e7e0c240de66c5b
Andrew Wray <andrewwray2718@gmail.com>
2020-08-12 12:20:43 -0700

    Add Flatten layer

--- models.py
+++ models.py
@@ -24,51 +24,52 @@ def create_logreg(input_shape, num_genres=NUM_GENRES):
     return model
 
 
-def create_CNN(input_shape, num_genres=NUM_GENRES):
+def create_CNN(input_shape, filters=32, num_genres=NUM_GENRES):
 
     model = keras.Sequential()
 
     # conv layer 1
     model.add(
         keras.layers.Conv2D(
-            num_filters,
-            kernel_size=kernel_size,
+            filters=filters,
+            kernel_size=(3, 3),
             activation="relu",
             input_shape=input_shape,
             kernel_regularizer=keras.regularizers.l2(0.001),
         )
     )
-    model.add(keras.layers.MaxPool2D(pool_size=pool_size))
+    model.add(keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
     # model.add(keras.layers.Dropout(0.2))
 
     # conv layer 2
     model.add(
         keras.layers.Conv2D(
-            num_filters,
-            kernel_size=kernel_size,
+            filters=filters,
+            kernel_size=(3, 3),
             activation="relu",
             kernel_regularizer=keras.regularizers.l2(0.001),
         )
     )
-    model.add(keras.layers.MaxPool2D(pool_size=pool_size))
+    model.add(keras.layers.MaxPool2D(pool_size=(3, 3), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
     # model.add(keras.layers.Dropout(0.2))
 
     # conv layer 3
     model.add(
         keras.layers.Conv2D(
-            num_filters,
-            kernel_size=kernel_size,
+            filters=filters,
+            kernel_size=(2, 2),
             activation="relu",
             kernel_regularizer=keras.regularizers.l2(0.001),
         )
     )
-    model.add(keras.layers.MaxPool2D(pool_size=pool_size))
+    model.add(keras.layers.MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding="same"))
     model.add(keras.layers.BatchNormalization())
     # model.add(keras.layers.Dropout(0.2))
 
     # flatten and dense layer
+    model.add(keras.layers.Flatten())
     model.add(keras.layers.Dense(64, activation="relu"))
     model.add(keras.layers.Dropout(0.3))
 
