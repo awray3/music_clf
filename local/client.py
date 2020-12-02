9cbf9546bf83b3300df9849376d1f1728b882ed8
Andrew Wray <andrewwray2718@gmail.com>
2020-12-02 13:13:03 -0800

    Fix filepath issue

--- local/client.py
+++ local/client.py
@@ -5,7 +5,7 @@ Client for the Flask server.
 import requests
 
 URL = "http://127.0.0.1:5000/predict"
-TEST_AUDIO_PATH = "./data/jazz/jazz.00001.wav"
+TEST_AUDIO_PATH = "./data/jazz/jazz.00005.wav"
 
 if __name__ == "__main__":
 
